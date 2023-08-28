bcb3f89b7be8035ec7b1f01124e03d500423a4bc
RamzesAI <ramzesru7@yandex.ru>
2023-08-28 22:59:47 +0300

    fix validation

--- src/car/dto/create-car.dto.ts
+++ src/car/dto/create-car.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsInt, IsString } from 'class-validator';
 
 export class CreateCarDto {
   @IsString({ message: 'Brand must be a string' })
@@ -7,9 +7,9 @@ export class CreateCarDto {
   @IsString({ message: 'Model must be a string' })
   name: string;
 
-  @IsNumber({}, { message: 'Year must be a number' })
+  @IsInt({ message: 'Year must be a number' })
   year: number;
 
-  @IsNumber({}, { message: 'Price must be a number' })
+  @IsInt({ message: 'Price must be a number' })
   price: number;
 }
